ed0407595726b56cd8a23754321863f5a1fa1140
joao <jcbalmeida@gmail.com>
2020-08-13 21:32:31 -0300

    Improves random slug generation

--- api/shortener/models.py
+++ api/shortener/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, IntegrityError
 from django.utils.timezone import now
 from datetime import timedelta
 from .utils import base62
@@ -7,6 +7,10 @@ from django.conf import settings
 
 def expires_at_default():
     return now() + timedelta(seconds=settings.SHORTENER_DEFAULT_DURATION)
+
+
+def get_random_slug(tries):
+    return base62.random(settings.SHORTENER_DEFAULT_LENGTH + tries)
 
 
 class Url(models.Model):
@@ -26,7 +30,16 @@ class Url(models.Model):
         return "{}/{}".format(settings.BASE_URL, self.slug)
 
     def save(self, *args, **kwargs):
-        if self._state.adding and not self.slug:
-            self.slug = base62.random(5)  # TODO: parametrizar
-
-        super().save(*args, **kwargs)
+        if (
+            self._state.adding and not self.slug
+        ):  # slug defaults to random string
+            for i in range(3):
+                self.slug = get_random_slug(i)
+                try:
+                    super().save(*args, **kwargs)
+                    return
+                except IntegrityError:
+                    continue
+            raise KeyError("Could not generate unique slug")
+        else:
+            super().save(*args, **kwargs)
