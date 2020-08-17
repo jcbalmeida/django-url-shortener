dc94daa172bbb65616a231a436f9e4aa3bdeff62
joao <jcbalmeida@gmail.com>
2020-08-17 05:08:06 -0300

    Patially implements login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,21 +1,47 @@
 <template>
   <div>
     <h1>Login</h1>
-    <input type="text" placeholder="username" />
+    <input type="text" v-model="username" placeholder="username" />
     <br />
-    <input type="password" placeholder="password" />
+    <input type="password" v-model="password" placeholder="password" />
     <br />
     <div>
-      <button>Login</button>
+      <button @click="login">Login</button>
     </div>
+    <ul v-if="errors && errors.length">
+      <li v-for="error in errors" :key="error">
+        {{ error.message }}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
   name: "login",
   data() {
-    return {};
+    return {
+      username: "",
+      password: "",
+      errors: []
+    };
   },
-  methods: {},
+  methods: {
+    login: function() {
+      axios
+        .post("http://localhost:8000/token/", {
+          username: this.username,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response);
+          this.$router.replace("/");
+        })
+        .catch(e => {
+          console.debug(e);
+        });
+    }
+  }
 };
 </script>
