c445bf485ac031c4edd07189457453d64aaf995f
joao <jcbalmeida@gmail.com>
2020-08-17 01:24:51 -0300

    Adds login form

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <input type="text" placeholder="username" />
+    <br />
+    <input type="password" placeholder="password" />
+    <br />
+    <div>
+      <button>Login</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
